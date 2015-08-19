8da6ee70769e4b7a6617ae49dcbf8fff385be5ef
Grizzlyt <grizzlyt@mail.ru>
2015-08-19 13:23:47 +0300

    Update Project.java

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -1,22 +1,40 @@
 package com.test;
-
+  //Add comment 2
+    //Add comment 2
+      //Add comment 2
+        //Add comment 2
+          //Add comment 2
+            //Add comment 2
+              //Add comment 2
 //dependency on Apache commons-collections
 import org.apache.commons.collections.Predicate;
-
+  //Add comment 2
+    //Add comment 2
+    //Add comment 2
 class Project {
+    //Add comment 2
+      //Add comment 2
+        //Add comment 2
+        
 // Add comment 1
   public static void test() {
     System.out.println("test");
   }
+    //Add comment 2
+      //Add comment 2
+        //Add comment 2
   //Add comment 2
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
-
+  //Add comment 2
 }
-
+  //Add comment 2
+  
+  
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
+  
